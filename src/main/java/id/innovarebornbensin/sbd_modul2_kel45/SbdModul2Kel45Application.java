f4b47fb2146db417fb5dc566a1b7e9042c34d2eb
Athari Muhammad <70275655+InnovaReborn2GD@users.noreply.github.com>
2026-03-01 00:22:53 +0700

    first commit

---
+++ src/main/java/id/innovarebornbensin/sbd_modul2_kel45/SbdModul2Kel45Application.java
@@ -0,0 +1,15 @@
+package id.innovarebornbensin.sbd_modul2_kel45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "id.innovarebornbensin.sbd_modul2_kel45")	
+public class SbdModul2Kel45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdModul2Kel45Application.class, args);
+	}
+
+}
---
+++ src/test/java/id/innovarebornbensin/sbd_modul2_kel45/SbdModul2Kel45ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.innovarebornbensin.sbd_modul2_kel45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdModul2Kel45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
